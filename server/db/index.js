9f7975ae607dffbdfb0bf409fa6631b4f64999dc
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-28 10:26:39 -0400

    Created db/index.js and set up mongoose connection

---
+++ server/db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+let dbURL = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/theLibraryOfMe'
+
+mongoose
+    .connect(dbURL)
+    .then(() => {
+        console.log('Successfully connected to MongoDB!')
+    })
+    .catch((e) => {
+        console.error('Connection error', e.message)
+    })
+mongoose.set('debug', true)
+const db = mongoose.connection
+
+module.exports = db
