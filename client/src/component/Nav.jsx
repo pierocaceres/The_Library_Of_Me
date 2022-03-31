e2c0fb1a2dd5f47cf218296a33727e7331031b39
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-31 15:19:22 -0400

    Updated and styled the nav bar.

--- client/src/component/Nav.jsx
+++ client/src/component/Nav.jsx
@@ -1,19 +1,20 @@
 import React, {useContext} from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { DataContext } from './DataContext'
 
 function Nav() {
 
     const {signin, user} = useContext(DataContext)
+    
+    let navigate = useNavigate()
 
     return (
         <div className="title-nav">
             <div className="website-title">
-                <h1>The Library of Me </h1>
+                <h1 onClick={() => navigate("/")}>The Library of Me</h1>
                 {/* <img src="../../../assets/bookshelf.jpg" alt='icon'/> */}
             </div>
             <div className="nav">
-                <Link to="/">Home</Link>
                 {!signin && <Link to="/signin">Sign in</Link>}
                 {signin && <Link to={`/${user.username}/my_library`}>My Library</Link>}
                 {signin && <Link to="/signout">Sign out</Link>}
