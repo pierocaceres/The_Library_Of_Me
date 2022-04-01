6db38663c414c5d1104e6d2f71bb10113040465a
Piero Caceres <pierocaceres92@yahoo.com>
2022-04-01 13:20:50 -0400

    Setup for Heroku deployment.

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-require('dotenv').config() 
+require('dotenv').config() // Add this line
 
 let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/theLibraryOfMe'
 
